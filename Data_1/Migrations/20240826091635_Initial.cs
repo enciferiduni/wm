acc067731d4f3339afb15fb28a102cc14b2eb540
asrajafari <168227642+asrajafari@users.noreply.github.com>
2024-08-28 06:50:17 -0700

    addd

---
+++ Data_1/Migrations/20240826091635_Initial.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data_1.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "categorys",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titel = table.Column<string>(type: "TEXT", nullable: false),
+                    TsActive = table.Column<bool>(type: "INTEGER", nullable: false),
+                    categoryid = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_categorys", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_categorys_categorys_categoryid",
+                        column: x => x.categoryid,
+                        principalTable: "categorys",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "todos",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Titel = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    IsCompleted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_todos", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_categorys_categoryid",
+                table: "categorys",
+                column: "categoryid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "categorys");
+
+            migrationBuilder.DropTable(
+                name: "todos");
+        }
+    }
+}
